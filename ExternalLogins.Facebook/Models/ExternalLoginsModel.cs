0b3dd1c8aaa7fa5590430bab250969142eb151b4
Brady Kelly <brady@bradykelly.net>
2017-08-04 19:44:46 +0200

    Added constants for FB creds. 	Thus postponed learning about Secret Manager.

---
+++ ExternalLogins.Facebook/Models/ExternalLoginsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExternalLogins.Facebook.Models
+{
+    public class ExternalLoginsModel
+    {
+        public class FaceBook
+        {
+            // NB Use Secret Manager, not these. Need to be ready for more complex auth.
+            public const string AppId = "1921033554817400";
+            public const string AppSecret = "18163913013fad1ba18490be1b20e473";
+        }
+    }
+}
