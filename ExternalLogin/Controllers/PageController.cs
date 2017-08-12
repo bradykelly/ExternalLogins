35570337bf656c1e010be780d0d9ada3fe8199fb
Brady Kelly <brady@bradykelly.net>
2017-08-12 18:36:02 +0200

    Getting there, Stopping to use User Secrets.

---
+++ ExternalLogin/Controllers/PageController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExternalLogin.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Home");
+        }
+
+        public ActionResult ThePage()
+        {
+            return View();
+        }
+    }
+}
