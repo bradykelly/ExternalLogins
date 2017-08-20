a9495fa0304e5a537538622cb22c59b3d4373ab9
Brady Kelly <brady@bradykelly.net>
2017-08-20 08:15:31 +0200

    Fixed a total cluster-fuck of a rollback.

---
+++ Code/ExternalLogins.Facebook/Data/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExternalLogins.Facebook.Data
+{
+    [Table("Booking")]
+    public class BookingDataModel
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public string Comment { get; set; }
+    }
+}
